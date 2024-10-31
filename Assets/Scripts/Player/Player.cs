d775fd802094fd837e1d917f65cf419c759b0d5a
hyobeen12 <aszx8683@naver.com>
2024-10-31 20:31:59 +0900

    [Add] Player
    Movement, Look, Jump(Jump는 수정예정)

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        CharacterManager.Instance.Player = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Move")]
+    public int moveSpeed;
+    private Vector2 curMoveInput;
+    public int jumpForce;
+    public LayerMask groundLayerMask;
+
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float minXRot;
+    public float maxXRot;
+    private float camCurXRot;
+    public float lookSensivity;
+
+
+    private Vector2 mouseDelta;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        Look();
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMoveInput.y + transform.right * curMoveInput.x;
+        dir *= moveSpeed;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+
+    void Look()
+    {
+        camCurXRot += mouseDelta.y * lookSensivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXRot, maxXRot);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensivity, 0);
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMoveInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMoveInput = Vector2.zero;
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && isGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool isGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray (transform.position + (transform.forward * 0.2f)+ ( transform.up * 0.01f),Vector2.down),
+            new Ray (transform.position + (-transform.forward * 0.2f)+ ( transform.up * 0.01f),Vector2.down),
+            new Ray (transform.position + (transform.right * 0.2f)+ ( transform.up * 0.01f),Vector2.down),
+            new Ray (transform.position + (-transform.right * 0.2f)+ ( transform.up * 0.01f),Vector2.down)
+        };
+
+        for(int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.5f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
