ebd8d65771419dea2c69444c35011b77fe4ff82b
devH <146154052+0umj@users.noreply.github.com>
2024-10-31 17:05:04 +0900

    feat: 목표 지점 기능 구현
    Description
    1. EndPoint 추상 클래스를 상속해 커스텀 목표 지점 생성 가능
    2. Tag: Player, Rigidbody 요소가 필수
    3. 목표 지점 도달 시 커스텀 메서드를 통해 기능 수행

---
+++ Assets/Scripts/Puzzle/SceneTransitionPoint.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionPoint : EndPoint
+{
+    private int nextSceneIndex; 
+
+    private void Start()
+    {
+        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+           // Debug.LogError("다음 씬이 존재하는지 확인 해주십시오.");
+            nextSceneIndex = SceneManager.GetActiveScene().buildIndex;  
+        }
+    }
+
+    protected override void HandleLevelComplete()
+    {
+        Debug.Log("클리어");
+       // SceneManager.LoadScene(nextSceneIndex);
+    }
+}
