bbbf6500c311fe305b4e1aae21e5d80d76d2cdce
tive1021 <tive1021@naver.com>
2024-10-31 19:56:58 +0900

    DataManager 추가
    LoadTopScene으로 가장 높은 Stage 로드
    SetTopScene으로 가장 높은 Stage 설정

---
+++ Assets/Scripts/Manager/DataManager.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataManager : Singleton<DataManager>
+{    
+    // 싱글톤 선언
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    // 달성한 Scene 정보를 저장할 데이터 클래스 정의
+    [System.Serializable]
+    public class TopSceneData
+    {
+        public string sceneName;
+    }
+
+    public void LoadTopScene()
+    {
+        // 파일 경로 지정
+        string filePath = "./topScene.json";
+
+        // 파일이 존재하지 않는다면
+        if (!File.Exists(filePath))
+        {
+            SetTopScene("Stage1");
+        }
+
+        // json파일 읽어서 Scene Load
+        string json = File.ReadAllText(filePath);
+        TopSceneData data = JsonUtility.FromJson<TopSceneData>(json);
+        SceneManager.LoadScene(data.sceneName);        
+    }
+
+    public void SetTopScene(string name)
+    {
+        // 매개변수 읽어서 json 작성
+        TopSceneData data = new TopSceneData { sceneName = name };
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText("./topScene.json", json);
+    }
+}
