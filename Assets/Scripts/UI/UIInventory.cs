6d736254bb247195c8feae15779cb93ffbb1b9a0
tive1021 <tive1021@naver.com>
2024-11-06 20:16:10 +0900

    key 연결

--- Assets/Scripts/UI/UIInventory.cs
+++ Assets/Scripts/UI/UIInventory.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -33,7 +34,6 @@ public class UIInventory : MonoBehaviour
         dropPosition = CharacterManager.Instance.Player.dropPosition;
 
         controller.inventory += Toggle;
-        CharacterManager.Instance.Player.addItem += AddItem;
 
         inventoryWindow.SetActive(false);
         slots = new ItemSlot[slotPanel.childCount];
@@ -74,10 +74,8 @@ public class UIInventory : MonoBehaviour
         return inventoryWindow.activeInHierarchy;
     }
     
-    void AddItem()
+    public void AddItem(ItemData data)
     {
-        ItemData data = CharacterManager.Instance.Player.itemData;
-
         ItemSlot emptySlot = GetEmptySlot();
 
         if(emptySlot != null)
@@ -85,13 +83,10 @@ public class UIInventory : MonoBehaviour
             emptySlot.item = data;
             emptySlot.quantity = 1;
             UpdateUI();
-            CharacterManager.Instance.Player.itemData = null;
             return;
         }
 
         ThrowItem(data);
-
-        CharacterManager.Instance.Player.itemData = null;
     }
 
     void UpdateUI()
