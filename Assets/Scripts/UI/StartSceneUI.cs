a97b4673e2ac4a090e243810f84d38e8ab34ce92
devH <146154052+0umj@users.noreply.github.com>
2024-11-07 00:48:00 +0900

    feat: 스타트 씬 기능 추가

--- Assets/Scripts/UI/StartSceneUI.cs
+++ Assets/Scripts/UI/StartSceneUI.cs
@@ -2,42 +2,70 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartSceneUI : MonoBehaviour
 {
-    [SerializeField] private Button  customModeStartButton;  
-    [SerializeField] private Button  storyModeStartButton;  
-    [SerializeField] private Button  settingButton;  
-    [SerializeField] private Button  exitButton;  
-    
-    
+    [Header("UI Componenets")] [SerializeField]
+    private Button customModeStartButton;
+
+    [SerializeField] private Button storyModeStartButton;
+    [SerializeField] private Button settingButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private GameObject customModePanel;
+    [SerializeField] private GameObject storyModePanel;
+
+
     private void Start()
     {
-        customModeStartButton.onClick.AddListener(OnCustomModeStart);    
-        storyModeStartButton.onClick.AddListener(OnStoryModeStart);    
-        settingButton.onClick.AddListener(OnSetting);    
-        exitButton.onClick.AddListener(OnExit);    
+        customModeStartButton.onClick.AddListener(OnCustomModeStart);
+        storyModeStartButton.onClick.AddListener(OnStoryModeStart);
+        settingButton.onClick.AddListener(OnSetting);
+        quitButton.onClick.AddListener(OnQuit);
     }
 
     private void OnCustomModeStart()
     {
-        
+        storyModePanel.SetActive(false);
+        customModePanel.SetActive(true);
     }
 
 
     private void OnStoryModeStart()
     {
-        
+        storyModePanel.SetActive(true);
+        customModePanel.SetActive(false);
     }
 
     private void OnSetting()
     {
-        
     }
 
-    private void OnExit()
+    private void OnQuit()
     {
         Application.Quit();
     }
+
+
+    public void OnContinueButton()
+    {
+        DataManager.Instance.LoadTopScene();
+    }
+
+    public void OnReStartButton()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void OnEditMapButton()
+    {
+        SceneManager.LoadScene("EditMapScene");
+    }
+    
+    public void OnPlayCustomMapButton()
+    {
+        SceneManager.LoadScene("PlayCustomGameScene");
+    }
+
 }
