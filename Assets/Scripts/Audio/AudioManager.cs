0c2efdeaf49ae8ad848e2492590ff9f39269cd7a
hyobeen12 <aszx8683@naver.com>
2024-11-07 11:37:49 +0900

    fix AudioManager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager instance;
+
     public AudioSource audioSource;
     public AudioClip clip;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
